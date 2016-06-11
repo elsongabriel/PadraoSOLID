d0214e3d28252410b40932f768267c59e735a474
Elson Costa <elsongabriel@hotmail.com>
2016-06-11 16:11:19 -0300

    Criando o projeto.

---
+++ src/br/com/egcservice/open_closed/Gerente.java
@@ -0,0 +1,34 @@
+package br.com.egcservice.open_closed;
+
+import br.com.egcservice.single_resposability.Cliente;
+
+/**
+ * Created by elson on 11/06/2016.
+ */
+public class Gerente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void abrirConta() {
+
+    }
+
+    public void fecharConta(int numConta) {
+
+    }
+
+    public void alterarInformacoesDoCliente(Cliente cliente) {
+
+    }
+
+    public void desativarCliente(Cliente cliente) {
+        
+    }
+}
